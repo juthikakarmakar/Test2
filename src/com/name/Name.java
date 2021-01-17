0870722b0c72e53f4f8419c7c3c5c8d898f63196
juthikakarmakar <juthikakarmakar1@gmail.com>
2021-01-17 19:24:12 +0530

    Name file added

---
+++ src/com/name/Name.java
@@ -0,0 +1,12 @@
+package com.name;
+
+public class Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=20,j=30,k=0;
+		k=i+j;
+		System.out.println(k);
+	}
+
+}
